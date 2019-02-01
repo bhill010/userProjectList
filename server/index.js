c8d46f205cca0ad7c8200a353606a80e9890a706
Brandon Hill <bhill010@gmail.com>
2019-02-01 12:41:23 -0800

    Begin setting up PORT environment variable for Heroku server deployment

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// Create environment variable so Heroku can input dynamic port
+// If there is no Heroku environment variable then use 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
